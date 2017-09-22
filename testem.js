7697386e1fea5f9fd591a46e2f7af2e6179dfe48
luqman <luqman.amjad@gmail.com>
2017-09-22 13:45:09 +0100

    ember cli upgrade 2.15.1 + circleci 2 setup

--- testem.js
+++ testem.js
@@ -1,12 +1,41 @@
 /* eslint-env node */
-module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+
+const path = require('path');
+const existsSync = require('exists-sync');
+const mkdirp = require('mkdirp');
+const isCI = !!process.env.CI;
+const scenario = process.env.EMBER_TRY_CURRENT_SCENARIO;
+
+let testReportsPath = 'test-results';
+let reportFile = path.join(testReportsPath, `${scenario}-test-results.xml`);
+
+if (existsSync(testReportsPath) === false) {
+  mkdirp.sync(testReportsPath);
+}
+
+let options = {
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  xunit_intermediate_output: true,
+  report_file: reportFile,
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: [
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 };
+
+if (isCI) {
+  options.reporter = 'xunit';
+}
+
+module.exports = options;
