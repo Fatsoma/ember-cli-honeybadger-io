71dbfb30032ca3a8c0dc35fc033dd761c9ae91b1
luqman <luqman@users.noreply.github.com>
2024-09-26 15:42:49 +0100

    remove files to get ready for fresh add-on blueprint files

--- app/services/honeybadger.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-cli-honeybadger-io/services/honeybadger';
--- tests/dummy/app/routes/ember-onerror.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  model() {
-    return new Error('javascript error');
-  }
-});
